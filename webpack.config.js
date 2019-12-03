8394cee45a918f7bdb4b2c4c7c02147a66490c04
mattdavey <matt.davey540@me.com>
2019-12-03 22:13:48 +0000

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-require('dotenv').config()
 
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve('dist'),
+    path: path.resolve(__dirname, 'src/dist'),
     publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
-      { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' }
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
     ]
   },
   devServer: {
     contentBase: path.resolve('src'),
     hot: true,
     open: true,
-    port: 8000,
+    port: 4000,
     watchContentBase: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
