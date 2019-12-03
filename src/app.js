d719a2fbbdd2880343f35f0dcd9b7e786d2e4245
mattdavey <matt.davey540@me.com>
2019-12-03 22:12:35 +0000

    pre checkout

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Landing from './src/components/common/Landing'
-import Navbar from './src/components/common/Navbar'
-import Book from './src/components/booking/Book'
+import Landing from './components/common/Landing'
+import Navbar from './components/common/Navbar'
+import Book from './components/booking/Book'
 
 import './style.scss'
 import 'normalize.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,36 @@
-// const path = require('path')
+const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const resolve = require('path').resolve
+require('dotenv').config()
 
 module.exports = {
-  entry: './app.js', 
+  entry: './src/app.js',
   output: {
-    // path: path.resolve(__dirname, './dist'),
-    path: resolve('dist'),
     filename: 'bundle.js',
+    path: path.resolve('dist'),
     publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' }
     ]
   },
   devServer: {
-    // contentBase: path.resolve('src'),
+    contentBase: path.resolve('src'),
     hot: true,
     open: true,
-    port: 4000,
+    port: 8000,
     watchContentBase: true,
-    historyApiFallback: true,
-    proxy: {
-      '/api': 'http://localhost:8000'
-    }
+    historyApiFallback: true
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: 'src/index.html',
       filename: 'index.html',
       inject: 'body'
     })
