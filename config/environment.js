95526ae79ef14a5d27e36f4e650b9132c298eadc
mattdavey <matt.davey540@me.com>
2020-05-07 15:42:12 +0100

    env db uri

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 8000
-const dbURI = 'mongodb://localhost/gf'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/gf'
 const secret = 'shh! not telling'
 
 module.exports = { dbURI, port, secret }
